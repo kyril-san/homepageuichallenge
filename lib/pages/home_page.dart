7d79812c1b67bd8a81b7b3f781bf255ec1c43bef
kyril-san <99323661+kyril-san@users.noreply.github.com>
2023-08-08 12:42:10 +0100

    Finished this Project

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, must_be_immutable
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, must_be_immutable, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -35,7 +35,27 @@ class HomePage extends StatelessWidget {
                           bottomRight: Radius.circular(40.r),
                         ),
                       ),
-                      child: profilepage(context),
+                      child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                                left: 21.w, top: 33.h, bottom: 19.h),
+                            child: IconButton(
+                                onPressed: () {
+                                  ShowModalBottomSheet(context);
+                                },
+                                icon: Icon(
+                                  Icons.menu,
+                                  color: Colors.white,
+                                )),
+                          ),
+                          Center(
+                            child: profilepage(context),
+                          )
+                        ],
+                      ),
                     ),
                     Positioned(
                       top: 277.h,
@@ -101,5 +121,109 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ));
+  }
+
+  Future<dynamic> ShowModalBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+        barrierColor: Color.fromARGB(255, 11, 12, 26).withOpacity(0.4),
+        context: context,
+        constraints: BoxConstraints(minHeight: 700.h),
+        builder: (_) {
+          return Container(
+            height: 700.h,
+            width: 375.w,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40.r),
+                    topRight: Radius.circular(40.r))),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.keyboard_arrow_down_outlined,
+                      // size: ,
+                      color: Color.fromARGB(255, 181, 182, 196),
+                    )),
+                SizedBox(
+                  height: 77.h,
+                ),
+                Text(
+                  '5:23 AM',
+                  style: TextStyle(
+                      fontSize: 64.sp,
+                      fontWeight: FontWeight.w700,
+                      color: const Color.fromARGB(255, 11, 12, 26)),
+                ),
+                Text(
+                  'Sunday,July 5',
+                  style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w600,
+                      color: const Color.fromARGB(255, 181, 182, 196)),
+                ),
+                Spacer(),
+                Container(
+                  width: 311.w,
+                  height: 64.h,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.r),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromARGB(255, 11, 12, 26)
+                                .withOpacity(0.05),
+                            offset: Offset(0, 1),
+                            blurRadius: 1,
+                            spreadRadius: 1)
+                      ]),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: ListTile(
+                        leading: Icon(
+                          Icons.bedtime,
+                          color: Colors.black,
+                        ),
+                        title: Text(
+                          'Deep Rest',
+                          style: TextStyle(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w600,
+                              color: const Color.fromARGB(255, 11, 12, 26)),
+                        ),
+                        subtitle: Text(
+                          'Rain on leaves',
+                          style: TextStyle(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w600,
+                              color: const Color.fromARGB(255, 181, 182, 196)),
+                        ),
+                      )),
+                      Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.fast_rewind),
+                            Icon(Icons.pause),
+                            Icon(Icons.fast_forward)
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 16.h,
+                )
+              ],
+            ),
+          );
+        });
   }
 }
